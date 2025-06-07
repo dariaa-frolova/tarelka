3ea3449c27ae956b05b4bbf2cacb7ef4b3ec4d60
dariaa-frolova <df7181841@gmail.com>
2025-06-07 08:49:51 +0700

    Инициализация проекта

---
+++ calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Калькулятор");
+            Console.WriteLine("1 - Сложение");
+            Console.WriteLine("2 - Вычитание");
+            Console.WriteLine("3 - Умножение");
+            Console.WriteLine("4 - Деление");
+
+            Console.Write("Выберите операцию: ");
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            
+        }
+    }
+}
